1e56b3d12769ce667ac77e046ba26a77fb414278
axocomm <axocomm@gmail.com>
2016-11-20 16:49:35 -0500

    Add status route with optional light parameter

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ app.config['HOST'] = '192.168.86.110'
 API_URL_FORMAT = 'http://%s/api/%s/%s'
 
 def api_url(sfx):
+    """Return the API URL for the given suffix."""
     return API_URL_FORMAT % (app.config['HOST'], app.config['USERNAME'], sfx)
 
 def get_status():
+    """Get status from the Hue base station.
+
+    Returns the HTTP status code and JSON response on 200, text otherwise"""
     r = requests.get(api_url('lights'))
     s = r.status_code
 
@@ -20,20 +24,32 @@ def get_status():
 
     return [s, r.text]
 
-@app.route('/')
-def index():
+@app.route('/status')
+@app.route('/status/<light>')
+def status(light=None):
     code, status = get_status()
-    if code == 200 and 'error' not in status:
-        response = {
-            'success': True,
-            'status': status
-        }
-    else:
-        response = {
+    if code != 200 or 'error' in status:
+        return jsonify({
             'success': False,
             'error': status
-        }
-    return jsonify(response)
+        })
+
+    if not light:
+        return jsonify({
+            'success': True,
+            'status': status
+        })
+    elif light in status:
+        return jsonify({
+            'success': True,
+            'light': light,
+            'status': status[light]
+        })
+    else:
+        return jsonify({
+            'success': False,
+            'error': 'Invalid light %s' % light
+        })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
