6c1350715c63f233dab56947e9b2f5918e3b4966
axocomm <axocomm@gmail.com>
2016-11-20 17:04:31 -0500

    Update jinja tags to fix Vue conflict

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ from flask import Flask, jsonify, render_template
 import json
 import requests
 
-app = Flask(__name__)
+class VueSafeFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='$$',
+        block_end_string='$$',
+        variable_start_string='$',
+        variable_end_string='$',
+        comment_start_string='$#',
+        comment_end_string='#$'
+    ))
+
+app = VueSafeFlask(__name__)
 app.config['USERNAME'] = 'xVPrnYHbkur99h0cSfDMGPXwft7wa4pVlmCemGxC'
 app.config['HOST'] = '192.168.86.110'
 
